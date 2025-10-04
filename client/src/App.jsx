ac7bf56d379821991f4ad718ea86b5e1573a9f00
FishShalami <fishshalami@gmail.com>
2025-10-04 00:14:14 -0700

    Added coord return in react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 // import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
-  fetch("http://localhost:3000/api");
+  function handleClick(event) {
+    const clickCoords = {
+      x: event.clientX,
+      y: event.clientY,
+    };
+    console.log(clickCoords);
+  }
 
   return (
     <>
-      <div>
+      <div className="image-container" onClick={handleClick}>
         <img
           src="http://localhost:3000/api"
           className="waldo-image"
