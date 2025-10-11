e938d34c024d6bf10ea375e77d8b5d71f1a481f6
FishShalami <fishshalami@gmail.com>
2025-10-10 21:19:17 -0700

    added components and new state variables for tracking user clicks and compare with waldo coord box

---
+++ client/src/utils/WALDO_COORDINATES.js
@@ -0,0 +1,9 @@
+import { range } from "./range";
+
+// x1 = 484, 167
+// x2 = 500, 167
+// x3 = 500, 189
+// x4 = 484, 189
+
+export const waldo_x_coordinates = range(484, 501);
+export const waldo_y_coordinates = range(167, 190);
