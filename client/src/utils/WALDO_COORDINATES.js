dcc8d7e842f1d85a0f8aa497d6068cf603cb0702
FishShalami <fishshalami@gmail.com>
2025-10-12 00:07:30 -0700

    Added relative positioning in the image selection for clicking on Waldo

--- client/src/utils/WALDO_COORDINATES.js
+++ client/src/utils/WALDO_COORDINATES.js
@@ -1,9 +1,10 @@
 import { range } from "./range";
 
-// x1 = 484, 167
-// x2 = 500, 167
-// x3 = 500, 189
-// x4 = 484, 189
+// x1 = 484, 167, .607, .349
 
-export const waldo_x_coordinates = range(484, 501);
-export const waldo_y_coordinates = range(167, 190);
+// x2 = 500, 167, .629, .349
+// x3 = 500, 189, .629, .392
+// x4 = 484, 189, .607, .392
+
+export const waldo_x_coordinates = range(607, 630, 1);
+export const waldo_y_coordinates = range(349, 393, 1);
--- todos.md
+++ todos.md
@@ -1,6 +1,5 @@
 **To-Dos**
 
-- Fix the position of the image so waldo always appears in same spot
 - Set-up back-end user time tracking and sessions
   - Collect user name on completion
 - Style the UI
