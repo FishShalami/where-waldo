9c2c004c5b031dbfe959e4b928006a0f6e429b1c
FishShalami <fishshalami@gmail.com>
2025-10-14 23:21:31 -0700

    Added main page basic header styling

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Where's Waldo?!</h1>
+      <p>Click the start button when ready to find Waldo</p>
+    </div>
+  );
+}
+
+export default Header;
